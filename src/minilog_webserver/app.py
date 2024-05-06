455061d0e5f4c9c12b181358423a96da630d4c50
Loic Riegel <external.loic.riegel@de.bosch.com>
2024-05-06 11:21:53 +0200

    feat: server displays received logs

--- src/minilog_webserver/app.py
+++ src/minilog_webserver/app.py
@@ -1,11 +1,45 @@
 """Entry point of the web server application."""
 
-from flask import Flask
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 
+received_logs: list[str] = [
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 - 1",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 - 2",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 - 3",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+    "127.0.0.1 - - [06/May/2024 10:17:46] GET /favicon.ico HTTP/1.1 404 -",
+]
+
+
+@app.route("/logs", methods=["GET"])
+def logs():
+    """Route to expose the received logs."""
+    return jsonify({"logs": received_logs})
+
+
 @app.route("/")
-def hello_world() -> None:
-    """Home route."""
-    return "<p>Hello, World!</p>"
+def hello_world():
+    """Home route that displays the logs."""
+    return render_template("log_page.html", messages=received_logs)
